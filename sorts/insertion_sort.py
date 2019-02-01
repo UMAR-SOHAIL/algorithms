639a9e610934708da1e870735b577e8f19a63ccc
Umar Sohail <umarsohail43742@hotmail.com>
2019-02-01 21:01:34 +0500

    memory optimized

--- sorts/insertion_sort.py
+++ sorts/insertion_sort.py
@@ -3,8 +3,7 @@
 def insertion_sort(ulist):
     length = len(ulist)
     for iteration in range(1, length):
-        key = iteration
-        while key > 0 and ulist[key -1] > ulist[key]:
-            ulist[key], ulist[key-1] = ulist[key-1], ulist[key]
-            key -= 1
+        while iteration > 0 and ulist[iteration -1] > ulist[iteration]:
+            ulist[iteration], ulist[iteration-1] = ulist[iteration-1], ulist[iteration]
+            iteration -= 1
     return ulist
