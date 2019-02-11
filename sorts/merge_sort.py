90137dd1e5fbe35d10a9f334b305dc50d0e295fa
Umar Sohail <umarsohail43742@hotmail.com>
2019-02-11 21:26:38 +0500

    if length is greter than 1 control flow added

--- sorts/merge_sort.py
+++ sorts/merge_sort.py
@@ -2,29 +2,30 @@
 
 def merge_sort(ulist):
     length = len(ulist)
-    division = length // 2
-    first_half = ulist[:division]
-    second_half = ulist[division:]
-    length_first_half = len(first_half)
-    length_second_half = len(second_half)
+    if length > 1:
+        division = length // 2
+        first_half = ulist[:division]
+        second_half = ulist[division:]
+        length_first_half = len(first_half)
+        length_second_half = len(second_half)
 
-    merge_sort(first_half)
-    merge_sort(second_half)
+        merge_sort(first_half)
+        merge_sort(second_half)
 
-    index = 0
-    left_index = 0
-    right_index = 0
+        index = 0
+        left_index = 0
+        right_index = 0
 
-    while left_index < length_first_half and right_index < length_second_half:
-        if first_half[left_index] < second_half[right_index]:
-            ulist[index] = first_half[left_index]
-            left_index += 1
-        else:
-            ulist[list] = second_half[right_index]
-            right_index += 1
-        index += 1
+        while left_index < length_first_half and right_index < length_second_half:
+            if first_half[left_index] < second_half[right_index]:
+                ulist[index] = first_half[left_index]
+                left_index += 1
+            else:
+                ulist[index] = second_half[right_index]
+                right_index += 1
+            index += 1
 
-    return ulist
+        return ulist
 
 
 
