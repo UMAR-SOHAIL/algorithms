51191039d73ee7c5463c1df017bce8b76a18aeec
Umar Sohail <umarsohail43742@hotmail.com>
2019-02-11 21:31:00 +0500

    loop added to stack remaining numbers on main list

--- sorts/merge_sort.py
+++ sorts/merge_sort.py
@@ -9,8 +9,8 @@ def merge_sort(ulist):
         length_first_half = len(first_half)
         length_second_half = len(second_half)
 
-        merge_sort(first_half)
-        merge_sort(second_half)
+        #merge_sort(first_half)
+        #merge_sort(second_half)
 
         index = 0
         left_index = 0
@@ -24,6 +24,17 @@ def merge_sort(ulist):
                 ulist[index] = second_half[right_index]
                 right_index += 1
             index += 1
+
+
+        while left_index < length_first_half:
+            ulist[index] = first_half[left_index]
+            left_index += 1
+
+
+        while right_index < length_second_half:
+            ulist[index] = second_half[right_index]
+            right_index += 1
+
 
         return ulist
 
