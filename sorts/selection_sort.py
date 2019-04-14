ad3791f277f265ce5bd6ad889a13012a6650a69b
Umar Sohail <umarsohail43742@hotmail.com>
2019-04-14 12:57:55 +0500

    selection sort optimized removed all unnessesary variables

--- sorts/selection_sort.py
+++ sorts/selection_sort.py
@@ -3,9 +3,7 @@
 def selection_sort(ulist):
     length = len(ulist)
     for iter in range(length):
-        index = iter
         for j in range(iter+1, length):
             if ulist[j] < ulist[iter]:
                 ulist[iter], ulist[j] = ulist[j], ulist[iter]
-    print(ulist)
     return ulist
