b218bc4c7aab328a2e0f27b277a786c982a5bbce
Umar Sohail <umarsohail43742@hotmail.com>
2019-04-14 12:56:14 +0500

    buged fixed for returning reversed list

--- sorts/selection_sort.py
+++ sorts/selection_sort.py
@@ -5,13 +5,7 @@ def selection_sort(ulist):
     for iter in range(length):
         index = iter
         for j in range(iter+1, length):
-            if ulist[j] > ulist[iter]:
+            if ulist[j] < ulist[iter]:
                 ulist[iter], ulist[j] = ulist[j], ulist[iter]
     print(ulist)
     return ulist
-
-
-#this part is for test purposes
-
-unorded = [3,2,1243,45,1,2,342,1,2,5,3,123,0, -1]
-selection_sort(unorded)
